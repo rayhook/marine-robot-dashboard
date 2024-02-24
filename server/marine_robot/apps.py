5b8d661f5e87898f19c9d6f76ad252485b3079c5
rayhook <koohyar@gmail.com>
2024-02-24 14:19:48 -0400

    Add marine robot config to django

--- server/marine_robot/apps.py
+++ server/marine_robot/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class MarineRobotConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'marine_robot'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "marine_robot"
