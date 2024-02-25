18798d37a7f868348f4a1729c7b4a8d97ebaa24f
rayhook <koohyar@gmail.com>
2024-02-25 17:16:46 -0400

    Extract api to seperate module

--- server/project/urls.py
+++ server/project/urls.py
@@ -1,19 +1,6 @@
 from django.contrib import admin
 from django.urls import path
-from ninja import NinjaAPI
-from ninja.security import django_auth
-
-api = NinjaAPI()
-
-
-@api.get("/add")
-def add(request, a: int, b: int):
-    return {"result": a + b}
-
-
-@api.get("pets", auth=django_auth)
-def pets(request):
-    return f"Autherization {request.auth}"
+from .api import api
 
 
 urlpatterns = [
