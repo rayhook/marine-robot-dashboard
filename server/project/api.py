32f9f26899bcd9fbcfc4d97019c6cbc9dafb9b6d
rayhook <koohyar@gmail.com>
2024-02-27 16:01:45 -0400

    Create Shema for device, trip, datapoint IN/OUT

--- server/project/api.py
+++ server/project/api.py
@@ -1,9 +1,12 @@
 from ninja import NinjaAPI, Schema
 from django.contrib.auth.hashers import make_password
 from django.contrib.auth import get_user_model
-
-from ninja_jwt.controller import NinjaJWTDefaultController
-
+from marine_robot.models import Device, Trip, Datapoint
+from decimal import Decimal
+from typing_extensions import Annotated
+from pydantic import Field
+from django.shortcuts import get_object_or_404
+from typing import List
 
 api = NinjaAPI()
 
@@ -12,6 +15,58 @@ class UserCreateSchema(Schema):
     username: str
     email: str
     password: str
+
+
+class DeviceCreateSchema(Schema):
+    total_distance: int
+    serial_number: str
+    time_since_last_grease: int
+    user_id: int
+
+
+class DeviceOutSchema(Schema):
+    id: int
+    total_distance: int
+    serial_number: str
+    time_since_last_grease: int
+    user_id: int
+
+
+class TripCreateSchema(Schema):
+    device_id: int
+    distance: int
+    duration: int
+
+
+class TripOutSchema(Schema):
+    id: int
+    device_id: int
+    distance: int
+    duration: int
+
+
+class DatapointCreateSchema(Schema):
+    trip_id: int
+    lat: Annotated[
+        Decimal,
+        Field(strict=False, allow_inf_nan=False, decimal_places=6, max_digits=9),
+    ]
+    long: Annotated[
+        Decimal,
+        Field(strict=False, allow_inf_nan=False, decimal_places=6, max_digits=9),
+    ]
+    depth: Annotated[
+        Decimal,
+        Field(strict=False, allow_inf_nan=False, decimal_places=2, max_digits=6),
+    ]
+    temp: Annotated[
+        Decimal,
+        Field(strict=False, allow_inf_nan=False, decimal_places=2, max_digits=5),
+    ]
+
+
+class AggTelemetrySchema(TripOutSchema):
+
 
 
 @api.post("/user/")
@@ -24,4 +79,45 @@ def create_user(request, payload: UserCreateSchema):
     )
     return {"id": user.id, "username": user.username, "email": user.email}
 
-@api.login
+
+@api.post("/device/")
+def create_device(request, payload: DeviceCreateSchema):
+    device = Device.objects.create(
+        total_distance=payload.total_distance,
+        serial_number=payload.serial_number,
+        time_since_last_grease=payload.time_since_last_grease,
+        user_id=1,
+    )
+    return {"id": device.id, "total_distance": device.total_distance}
+
+
+@api.post("/trip/")
+def create_trip(request, payload: TripCreateSchema):
+    trip = Trip.objects.create(
+        device_id=payload.device_id,
+        distance=payload.distance,
+        duration=payload.duration,
+    )
+
+    return {"id": trip.id, "device_id": trip.device_id}
+
+
+@api.post("/datapoint/")
+def create_trip(request, payload: DatapointCreateSchema):
+    datapoint = Datapoint.objects.create(
+        trip_id=payload.trip_id,
+        lat=payload.lat,
+        long=payload.long,
+        depth=payload.depth,
+        temp=payload.temp,
+    )
+
+    return {"id": datapoint.id, "device_id": datapoint.trip_id}
+
+
+@api.get("/aggdata/{device_id}", response=List[AggTelemetrySchema])
+def get_agg_data(request, device_id: int):
+    device = get_object_or_404(Device, id=device_id)
+    trips = Trip.objects.filter(device_id=device.id)
+
+    return trips
