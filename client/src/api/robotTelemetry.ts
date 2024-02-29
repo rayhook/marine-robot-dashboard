5a0307ced2c7856a23e562e6960986357e9da259
rayhook <koohyar@gmail.com>
2024-02-29 13:32:55 -0400

    Extract fetch request into a seperate module

---
+++ client/src/api/robotTelemetry.ts
@@ -0,0 +1,8 @@
+import axios from "axios";
+export const fetchcomData = async (deviceId: number) => {
+  const response = await axios({
+    method: "get",
+    url: `http://127.0.0.1:8000/api/comdata/${deviceId}`,
+  });
+  return response.data;
+};
--- client/src/components/Dashboard.tsx
+++ client/src/components/Dashboard.tsx
@@ -1,16 +1,39 @@
 import { useQuery } from "@tanstack/react-query";
+import { fetchcomData } from "../api/robotTelemetry";
 
 const Dashboard = () => {
+  const deviceId = 1;
   const { isPending, error, data } = useQuery({
-    queryKey: ["data"],
-    queryFn: () =>
-      fetch("http://127.0.0.1:8000/api/comdata/1").then((response) =>
-        response.json()
-      ),
+    queryKey: ["data", deviceId],
+    queryFn: async () => await fetchcomData(deviceId),
   });
 
   if (isPending) return "Loading...";
   if (error) return "An error has occured" + error.message;
-  return <div> total distance: {data.total_distance}</div>;
+  return (
+    <div className="min-h-screen">
+      <Stats data={data} />
+    </div>
+  );
 };
 export default Dashboard;
+
+interface DataType {
+  id: number;
+  time_since_last_grease: number;
+  total_distance: number;
+  serial_number: string;
+  user_id: number;
+}
+const Stats = ({ data }: { data: DataType }) => {
+  return (
+    <div className="container min-h-screen flex justify-between p-3">
+      <div className="bg-red-300 flex flex-col w-1/3 text-center">
+        <div>User{data.user_id}</div>
+        <div>Travelled: {data.total_distance} meters</div>
+        <div>Robot serial_number: {data.serial_number}</div>
+      </div>
+      <div className="bg-orange-300 w-2/3">Charts and map</div>
+    </div>
+  );
+};
