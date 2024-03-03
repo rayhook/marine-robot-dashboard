fde588ccbbdf67ff20b9257cd6e9967904772ac1
rayhook <koohyar@gmail.com>
2024-03-03 17:06:59 -0400

    Fetch datapoints and toggle dropdown

--- client/src/api/TripDatapoints.ts
+++ client/src/api/TripDatapoints.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 export const fetchDatapoints = async (trip_id: number) => {
   const response = await axios({
     method: "get",
-    url: `http://localhost:8000/api/datapoint/${trip_id}`,
+    url: `http://localhost:8000/api/datapoints/${trip_id}`,
   });
   return response.data;
 };
--- client/src/components/Dashboard.tsx
+++ client/src/components/Dashboard.tsx
@@ -1,8 +1,9 @@
 import { useQuery } from "@tanstack/react-query";
 import { getDeviceData } from "../api/marineTelemetry";
+import { fetchDatapoints } from "../api/TripDatapoints";
+import { useState } from "react";
 
 const Dashboard = () => {
-  const deviceId = 1;
   const { isPending, error, data } = useQuery({
     queryKey: ["data", deviceId],
     queryFn: async () => await getDeviceData(deviceId),
@@ -10,27 +11,63 @@ const Dashboard = () => {
 
   if (isPending) return "Loading...";
   if (error) return "An error has occured" + error.message;
-  return <Stats data={data} />;
+  return <Stats deviceData={data} />;
 };
 export default Dashboard;
 
-interface DataType {
+interface TripType {
+  id: number;
+  device_id: number;
+  distance: number;
+  duration: number;
+}
+
+interface DeviceDataType {
   id: number;
   time_since_last_grease: number;
   total_distance: number;
   serial_number: string;
   user_id: number;
+  trips: TripType[];
 }
 
-const Stats = ({ data }: { data: DataType }) => {
+const Stats = ({ deviceData }: { deviceData: DeviceDataType }) => {
+  const [showDropDown, setShowDropDown] = useState(false);
+  const [tripId, setTripId] = useState(null);
+
+  const handleTripSelect = async (tripId: number) => {
+    setShowDropDown((prevState: boolean) => !prevState);
+  };
+
+  const { isPending, error, dataPoints } = useQuery({
+    queryKey: ["dataPoint", tripId],
+    queryFn: async () => fetchDatapoints(tripId),
+  });
+
   return (
     <div className="container min-h-screen flex justify-between">
       <div className="bg-red-300 flex flex-col w-1/3 text-center">
-        <div>User{data.user_id}</div>
-        <div>Travelled: {data.total_distance} meters</div>
-        <div>Robot serial_number: {data.serial_number}</div>
+        <div>User{deviceData.user_id}</div>
+        <div>Travelled: {deviceData.total_distance} meters</div>
+        <div>Robot serial_number: {deviceData.serial_number}</div>
       </div>
-      <div className="bg-orange-300 w-2/3">Charts and map</div>
+      <div className="bg-orange-300 w-2/3 flex">
+        <div>
+          {deviceData.trips.map((trip) => (
+            <div
+              className="flex gap-2 bg-slate-300 py-4"
+              onClick={() => handleTripSelect(trip.id)}
+            >
+              <div>trip id {trip.id}</div>
+              <div>trip distance {trip.distance}</div>
+              <div>trip duration {trip.duration}</div>
+              {showDropDown && (
+                <div className="">{isPending && <>isLoading...</>}</div>
+              )}
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
