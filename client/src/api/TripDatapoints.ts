663d8f9c81d31582dd8af47226f4c5a3a2f0fcaf
rayhook <koohyar@gmail.com>
2024-03-05 07:24:29 -0400

    Handle if argument is null

--- client/src/api/TripDatapoints.ts
+++ client/src/api/TripDatapoints.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 export const fetchDatapoints = async (trip_id: number) => {
+  if (!trip_id) return;
   const response = await axios({
     method: "get",
     url: `http://localhost:8000/api/datapoints/${trip_id}`,
